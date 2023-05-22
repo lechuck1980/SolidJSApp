0c30f9890448d8b0702c4253b5e028071df2801d
lechuck1980 <ciano_80@hotmail.com>
2023-05-22 19:03:37 +0200

    setup solidjs/router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 
+import styles from "./App.module.css";
+import { A } from "@solidjs/router";
+
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <div >
+        <header >
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            
+            href="https://github.com/solidjs/solid"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Solid
+          </a>
+          <A href="/about">About</A> 
+          <A href="/contact">Contact</A>
+          
+        </header>
+      </div>
     </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,34 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
+import { lazy } from "solid-js";
+import "./index.css";
+import App from "./App";
 
-import './index.css';
-import App from './App';
+const About = lazy(() => import("./components/About"));
+const Contact = lazy(() => import("./components/Contact"));
 
-const root = document.getElementById('root');
+import { Router, Routes, Route } from "@solidjs/router";
+
+const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?',
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?"
   );
 }
 
-render(() => <App />, root);
+render(
+  () => (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" component={App} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
+        <App />
+      </Router>
+    </>
+  ),
+  root
+);
